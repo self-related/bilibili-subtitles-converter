0dd94f20b8fbbaa9cabdfb78c73844208bb9e163
Nekit <nick.misiura@yandex.ru>
2026-06-26 13:19:26 +0300

    Ref: move convertion into converter module

--- index.js
+++ index.js
@@ -1,76 +1,32 @@
-/**@type {HTMLInputElement} */
+import { convertFromJson } from "./scripts/converter.js";
+
+
+/**@type { HTMLInputElement } */
 const fileInput = document.getElementById("file-input");
 
-/**@type {HTMLButtonElement} */
+/**@type { HTMLButtonElement } */
 const convertBtn = document.getElementById("convert-btn");
 
-/**@type {HTMLParagraphElement} */
+/**@type { HTMLParagraphElement } */
 const resultTextElement = document.getElementById("result-text");
 
-/**@type {HTMLAnchorElement} */
+/**@type { HTMLAnchorElement } */
 const downloadBtn = document.getElementById("download-btn");
 
-/**@type {HTMLInputElement} */
+/**@type { HTMLInputElement } */
 const langInput = document.getElementById("lang-input");
 
 
-let downloadBlob = new Blob();
+/**@type { Blob } */
+let downloadBlob = null;
 let convertedSrt = "";
 let language = langInput.value;
 
 
-/**@type {string[]} */
-const srtTimelines = [];
-
-
-/**@param {number} oldSeconds */
-function getTimeFromSeconds(oldSeconds) {
-    const milisecondsStr = (oldSeconds - (oldSeconds << 0)).toFixed(3).slice(2);
-    const fullSeconds = oldSeconds << 0;
-    
-    let seconds = 0;
-    let secondsStr = "00";
-
-    let minutes = 0;
-    let minutesStr = "00";
-
-    let hours = 0;
-    let hoursStr = "00";
-
-    if (fullSeconds < 60) {
-        seconds = fullSeconds;
-    } else {
-        minutes = (fullSeconds / 60) << 0;
-        seconds = fullSeconds - minutes * 60;
-    }
-
-    if (minutes > 59) {
-        hours = (minutes / 60) << 0;
-        minutes = minutes - hours * 60
-    }
-
-    secondsStr = (seconds > 9) ? `${seconds}` : `0${seconds}`;
-    minutesStr = (minutes > 9) ? `${minutes}` : `0${minutes}`;
-    hoursStr = (hours > 9) ? `${hours}` : `0${hours}`;
- 
-    return `${hoursStr}:${minutesStr}:${secondsStr}.${milisecondsStr}`;
-}
-
 convertBtn.addEventListener("click", async () => {
-    const pickedJson = await fileInput.files[0].text();
-    const pickedObj = JSON.parse(pickedJson);
-
-    /**@type {Array<{from: number, to: number, content: string}>} */
-    const pickedObjBody = pickedObj.body;
-
-    pickedObjBody.forEach((value) => {
-        const timeFrom = getTimeFromSeconds(value.from);
-        const timeTo = getTimeFromSeconds(value.to);
-        srtTimelines.push(`${timeFrom} --> ${timeTo}\n${value.content}`);
-
-    });
-
-    convertedSrt = `WEBVTT\nKind: captions\nLanguage: ${language}\n\n`+ srtTimelines.join('\n\n');
+    const json = await fileInput.files[0].text();
+    convertedSrt = convertFromJson(json, language);
+    
     resultTextElement.innerText = convertedSrt;
     downloadBlob = new Blob([convertedSrt], { type: "text/plain" });
 });
---
+++ scripts/converter.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+/** Types
+ * @typedef {{ from: number, to: number, content: string }} Timeline - timelines in JSON's body array
+ * @typedef {{ body: Timeline[] }} JsonSubs - type for JSON
+ */
+
+
+/**@type { function(number): string } */
+function getFullTimeFromSeconds(timeInSeconds) {
+    let seconds = ~~(timeInSeconds); // drop decimal part
+    const milis = timeInSeconds - seconds; // milis
+
+    let minutes = ~~(seconds / 60);
+    seconds -= minutes * 60;
+
+    let hours = ~~(minutes / 60);
+    minutes -= hours * 60;
+
+    const milisStr = milis.toFixed(3).slice(2);
+    let secondsStr = (seconds > 9) ? `${seconds}` : `0${seconds}`;
+    let minutesStr = (minutes > 9) ? `${minutes}` : `0${minutes}`;
+    let hoursStr = (hours > 9) ? `${hours}` : `0${hours}`;
+ 
+    return `${hoursStr}:${minutesStr}:${secondsStr}.${milisStr}`;
+}
+
+
+/**@type { function(string, string): string }*/
+export function convertFromJson(json, language) {
+    /**@type { string[] } */
+    const convertedTimelines = [];
+
+    /**@type { JsonSubs } */
+    const subs = JSON.parse(json);
+    const timelines = subs.body;
+
+    for (const timeline of timelines) {
+        const timeFrom = getFullTimeFromSeconds(timeline.from);
+        const timeTo = getFullTimeFromSeconds(timeline.to);
+        convertedTimelines.push(`${timeFrom} --> ${timeTo}\n${timeline.content}`);
+    }
+
+    return `WEBVTT\nKind: captions\nLanguage: ${language}\n\n` + convertedTimelines.join('\n\n');
+}
